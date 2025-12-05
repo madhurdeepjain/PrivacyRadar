548ace7d3c87f18cfb134ae011c897f29fa79fe5
Luke Therieau <luketherieau@outlook.com>
2025-12-05 18:13:30 -0500

    feat: Dark Mode/Light Mode and Navigation between Advanced and Basic Mode

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -8,10 +8,33 @@ import logo from '../../../../resources/icon.png'
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   currentView: 'network' | 'system'
   onViewChange: (view: 'network' | 'system') => void
+  advancedMode: boolean
+  setAdvancedMode: React.Dispatch<React.SetStateAction<boolean>>
+  darkMode: boolean
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export function Sidebar({ className, currentView, onViewChange }: SidebarProps): React.JSX.Element {
+export function Sidebar({
+  className,
+  currentView,
+  onViewChange,
+  advancedMode,
+  setAdvancedMode,
+  darkMode,
+  setDarkMode
+}: SidebarProps): React.JSX.Element {
   const [isCollapsed, setIsCollapsed] = useState(false)
+  const [showSettings, setShowSettings] = useState(false)
+
+  const handleDarkModeChange = (): void => {
+    setDarkMode(!darkMode)
+    window.api.setValue('darkMode', (!darkMode).toString())
+  }
+
+  const handleAdvancedModeChange = (): void => {
+    setAdvancedMode(!advancedMode)
+    window.api.setValue('advancedMode', (!advancedMode).toString())
+  }
 
   return (
     <div
@@ -43,7 +66,7 @@ export function Sidebar({ className, currentView, onViewChange }: SidebarProps):
           onClick={() => setIsCollapsed(!isCollapsed)}
           title={isCollapsed ? 'Expand Sidebar' : 'Collapse Sidebar'}
         >
-          {isCollapsed ? (
+          {!isCollapsed || !showSettings ? (
             <PanelLeftOpen className="h-4 w-4" />
           ) : (
             <PanelLeftClose className="h-4 w-4" />
@@ -81,11 +104,84 @@ export function Sidebar({ className, currentView, onViewChange }: SidebarProps):
           <Button
             variant="ghost"
             className={cn('w-full', isCollapsed ? 'justify-center px-2' : 'justify-start')}
-            title={isCollapsed ? 'Preferences' : undefined}
+            title={isCollapsed ? 'Please open the sidebar to access Preferences' : undefined}
+            onClick={() => {
+              if (isCollapsed) setShowSettings(true)
+              if (!isCollapsed) setShowSettings(!showSettings)
+              if (isCollapsed) setIsCollapsed(false)
+            }}
           >
             <Settings className={cn('h-4 w-4', !isCollapsed && 'mr-2')} />
             {!isCollapsed && 'Preferences'}
           </Button>
+          {!isCollapsed && showSettings && (
+            <div className="gap-4 shrink-0 animate-in slide-in-from-top-2 fade-in duration-200">
+              <div>
+                <label
+                  className={cn(
+                    'themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center',
+                    !isCollapsed && 'mr-2'
+                  )}
+                >
+                  <input
+                    type="checkbox"
+                    checked={darkMode}
+                    onChange={handleDarkModeChange}
+                    className="sr-only"
+                  />
+                  <span className={cn('h-4 w-4', !isCollapsed && 'mr-2')}>
+                    {!isCollapsed && 'Light'}
+                  </span>
+                  <span
+                    className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
+                      darkMode ? 'bg-[#212b36]' : 'bg-[#CCCCCE]'
+                    }`}
+                  >
+                    <span
+                      className={`dot h-6 w-6 rounded-full bg-white duration-200 ${
+                        darkMode ? 'translate-x-[28px]' : ''
+                      }`}
+                    ></span>
+                  </span>
+                  <span className={cn('h-4 w-4', !isCollapsed && 'mr-2')}>
+                    {!isCollapsed && 'Dark'}
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label
+                  className={cn(
+                    'themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center',
+                    !isCollapsed && 'mr-2'
+                  )}
+                >
+                  <input
+                    type="checkbox"
+                    checked={advancedMode}
+                    onChange={handleAdvancedModeChange}
+                    className="sr-only"
+                  />
+                  <span className={cn('h-4 w-4', !isCollapsed && 'mr-2')}>
+                    {!isCollapsed && 'Basic'}
+                  </span>
+                  <span
+                    className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
+                      advancedMode ? 'bg-[#212b36]' : 'bg-[#CCCCCE]'
+                    }`}
+                  >
+                    <span
+                      className={`dot h-6 w-6 rounded-full bg-white duration-200 ${
+                        advancedMode ? 'translate-x-[28px]' : ''
+                      }`}
+                    ></span>
+                  </span>
+                  <span className={cn('h-4 w-4', !isCollapsed && 'mr-2')}>
+                    {!isCollapsed && 'Advanced'}
+                  </span>
+                </label>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
